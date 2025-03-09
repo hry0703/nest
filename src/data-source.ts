8f646bc50ce822cac3904764bbc8cb8f960cf26c
Eden.huang <rongyao.huang@connext.com.cn>
2025-03-09 22:33:17 +0800

    Tree

--- src/data-source.ts
+++ src/data-source.ts
@@ -1,7 +1,8 @@
 import { DataSource } from 'typeorm'
-import { User } from './entity/user'
-import { Order } from './entity/order'
-import { Role } from './entity/role'
+// import { User } from './entity/user'
+// import { Order } from './entity/order'
+// import { Role } from './entity/role'
+import { Category } from './entity/category'
 // import { Profile } from './entity/profile'
 export const AppDataSource = new DataSource({
   type: 'mysql',
@@ -11,7 +12,7 @@ export const AppDataSource = new DataSource({
   password: 'admin123456',
   database: 'orm',
   synchronize: true, // 是否自动同步实体与数据库的表结构 开发时为true 生产时为false
-  logging: true, // 是否打印sql日志
-  entities: [User,Role], // 实体类数组 指定的是要用到的实体类
+  logging: false, // 是否打印sql日志
+  entities: [Category], // 实体类数组 指定的是要用到的实体类
 //   entities: ["entity/*.ts"], // 实体类数组 指定的是要用到的实体类
 })
--- src/index.ts
+++ src/index.ts
@@ -1,29 +1,61 @@
 import { AppDataSource } from "./data-source";
-import { Order } from "./entity/order";
-import { Role } from "./entity/role";
+import { Category } from "./entity/category";
+// import { Order } from "./entity/order";
+// import { Role } from "./entity/role";
 // import { Profile } from "./entity/Profile";
-import { User } from "./entity/user";
+// import { User } from "./entity/user";
 
 AppDataSource.initialize().then(async connection => {
-    const userRepository = AppDataSource.getRepository(User)
-    const roleRepository = AppDataSource.getRepository(Role)
+    const categoryRespository = connection.getTreeRepository(Category)
+    const root = new  Category()
+    root.name = 'root'
+    await categoryRespository.save(root)
 
-    const role1 = new Role()
-    role1.name = 'admin'
-    await roleRepository.save(role1)  // 若user实体中roles列设置cascade:true 则可以直接保存role1、role2 不需要再做这步手动保存role1、role2
+    const child1 = new  Category()
+    child1.name = 'child1'
+    child1.parent = root
+    await categoryRespository.save(child1)
 
-    const role2 = new Role()
-    role2.name = 'User'
-    await roleRepository.save(role2)
+    const grandChild1 = new  Category()
+    grandChild1.name = 'child1-grandChild1'
+    grandChild1.parent = child1
+    await categoryRespository.save(grandChild1)
 
 
-    const user1 = new User()
-    user1.firstName = 'Eden'
-    user1.lastName = 'Huang'
-    user1.age = 32
-    user1.email = '1@qq.com'
-    user1.isActive = true
-    user1.roles = [role1,role2]
-    await userRepository.save(user1)
+    const child2 = new  Category()
+    child2.name = 'child2'
+    child2.parent = root
+    await categoryRespository.save(child2)
+
+
+    // 查询所有的分类
+    const find = await categoryRespository.find()
+    console.log('find',find);
+    // 查询分类树
+    const findTrees = await categoryRespository.findTrees()
+    console.log('findTrees',findTrees);
+    // 查询根分类
+    const findRoots = await categoryRespository.findRoots()
+    console.log('findRoots',findRoots);
+    // 查询祖先树
+    const findAncestorsTree = await categoryRespository.findAncestorsTree(grandChild1)
+    console.log('findAncestorsTree',findAncestorsTree);
+    // 查询祖先
+    const findAncestors = await categoryRespository.findAncestors(grandChild1)
+    console.log('findAncestors',findAncestors);
+    // 查询后代树
+    const findDescendantsTree = await categoryRespository.findDescendantsTree(root)
+    console.log('findDescendantsTree',findDescendantsTree);
+    // 查询后代
+    const findDescendants = await categoryRespository.findDescendants(root)
+    console.log('findDescendants',findDescendants);
+    // 查询后代数量
+    const countDescendants = await categoryRespository.countDescendants(root)
+    console.log('countDescendants',countDescendants);
+    // 查询祖先数量
+    const countAncestors = await categoryRespository.countAncestors(grandChild1)
+    console.log('countAncestors',countAncestors);
+    
+
 
 }).finally(()=>process.exit(0)) // 关闭数据库连接
